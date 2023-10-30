e90477c2744eb6dee577ed7ed1f965789afc5d4e
koro <drganeelmegumin23@gmail.com>
2023-10-29 17:18:09 -0700

    installed prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    ignorePatterns: ['dist/', '.eslintrc.cjs']
+}
