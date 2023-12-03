67b43cf4cb0c34d3107da9ff36d967fa11a58972
JosiahFu <josiahfu@gmail.com>
2023-12-03 14:22:47 -0800

    Add dev script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
 import { startDockerContainer, stopDockerContainer } from 'database';
 
+// If DEV is true then the app should forward requests to localhost:5173 instead of serving from /static
+const DEV = process.env.NODE_ENV === 'dev';
+
 const container = await startDockerContainer('socal-db');
+console.log(DEV ? 'Developemnt' : 'Production');
 await stopDockerContainer(container);
 console.log('Done');
