3cc3024a213db4ed21123a3b90f7d0b727fa833a
koro <drganeelmegumin23@gmail.com>
2023-12-03 18:04:13 -0800

    forward request during dev

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import proxy from 'express-http-proxy';
 
 // If DEV is true then the app should forward requests to localhost:5173 instead of serving from /static
 const DEV = process.env.NODE_ENV === 'dev';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+if (DEV) {
+    app.use(
+        '/',
+        proxy('http://localhost:5173', { proxyReqPathResolver: e => e.url })
+    );
+} else {
+    app.use(express.static('static'));
+}
 
 export { app };
