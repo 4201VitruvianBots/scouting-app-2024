723163a30d27e0e9f16ee282aa5394bfcd89555c
Aiden Corral <aidencorral316@gmail.com>
2023-12-03 17:31:56 -0800

    added funny sound buttons (Links to other apps)!

--- client/src/RootMenu.tsx
+++ client/src/RootMenu.tsx
@@ -1,11 +1,26 @@
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function RootMenu() {
+    const foilage = new Audio("/src/audio/vine-boom.mp3")
+
+    const trum = new Audio("/src/audio/trumpet.mp3")
+
+    const poo = () => {
+        foilage.play()
+    }
+
+    const po = () => {
+        trum.play()
+    }
+
     return (
         <main>
-            
+            <button className='text-red-800 text-2xl font-black cursor-not-allowed underline' onClick={poo}><Link to='/match'>VINE BOOM</Link></button>
+            <button className='text-green-700 text-2xl font-serif cursor-wait underline' onClick={po}><Link to='/super'>Trumpet!</Link></button>
         </main>
     );
 }
+
+
 
 export default RootMenu;
