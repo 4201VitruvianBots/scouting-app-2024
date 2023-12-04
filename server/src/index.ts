703296f95dd5ec9791d36d0033a4474d3834a2f8
JosiahFu <josiahfu@gmail.com>
2023-12-03 22:07:49 -0800

    Fix stopping error

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,17 @@ const container = await startDockerContainer('socal-db');
 const server = app.listen(8080, () => {
     console.log('Server running on port 8080');
 });
-process.on('SIGINT', async () => {
-    console.log('stopping server...');
-    server.close();
 
+let stopping = false;
+
+process.on('SIGINT', async () => {
+    if (stopping) return;
+    stopping = true;
+
+    console.log('Stopping server...');
+    server.close();
     await stopDockerContainer(container);
+
     console.log('Done');
     process.exit();
 });
