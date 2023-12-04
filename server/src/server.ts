6df59cd06b5e08f03a73384f827b35e09a50ffe1
JosiahFu <josiahfu@gmail.com>
2023-12-03 21:59:47 -0800

    Fix proxy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
 import express from 'express';
-import proxy from 'express-http-proxy';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // If DEV is true then the app should forward requests to localhost:5173 instead of serving from /static
 const DEV = process.env.NODE_ENV === 'dev';
 
 const app = express();
 
+// Since this is the fallback is must go after all other routes
 if (DEV) {
     app.use(
         '/',
-        proxy('http://localhost:5173', { proxyReqPathResolver: e => e.url })
+        createProxyMiddleware('http://localhost:5173', { ws: true })
     );
 } else {
     app.use(express.static('static'));
