b4eaaf600be9e004cd2565d38c5e0e60ed4e89ea
Crisanto Carrillo <crisanto.e.carrillo@gmail.com>
2023-12-05 11:16:27 -0800

    Added comments

--- client/src/RootMenu.tsx
+++ client/src/RootMenu.tsx
@@ -4,18 +4,6 @@ import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
 import ScreenSearchDesktopIcon from '@mui/icons-material/ScreenSearchDesktop';
 
 function RootMenu() {
-    // const foilage = new Audio("/src/audio/vine-boom.mp3")
-
-    // const trum = new Audio("/src/audio/trumpet.mp3")
-
-    // const poo = () => {
-    //     foilage.play()
-    // }
-
-    // const po = () => {
-    //     trum.play()
-    // }
-
     return (
         <main className='box-border grid h-screen grid-cols-2 items-stretch gap-5 p-5'>
             <Link to={'/match'} className='inline-block'>
@@ -36,13 +24,26 @@ function RootMenu() {
                     Super
                 </Button>
             </Link>
-        {/* <main>
-            <button className='text-red-800 text-2xl font-black cursor-not-allowed underline' onClick={poo}><Link to='/match'>VINE BOOM</Link></button>
-            <button className='text-green-700 text-2xl font-serif cursor-wait underline' onClick={po}><Link to='/super'>Trumpet!</Link></button> */}
         </main>
     );
 }
 
-
-
 export default RootMenu;
+
+
+// Christian's Funny Buttons vvv
+    // const foilage = new Audio("/src/audio/vine-boom.mp3")
+
+    // const trum = new Audio("/src/audio/trumpet.mp3")
+
+    // const poo = () => {
+    //     foilage.play()
+    // }
+
+    // const po = () => {
+    //     trum.play()
+    // }
+
+    {/* <main>
+    <button className='text-red-800 text-2xl font-black cursor-not-allowed underline' onClick={poo}><Link to='/match'>VINE BOOM</Link></button>
+    <button className='text-green-700 text-2xl font-serif cursor-wait underline' onClick={po}><Link to='/super'>Trumpet!</Link></button> */}
