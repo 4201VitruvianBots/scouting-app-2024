eaa9316dd39634f44f5b16a034bacd2ca9ad5dc6
natalie w <nmwpenguin@gmail.com>
2024-01-08 20:19:33 -0800

    Added field maps & toggle ability

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -1,7 +1,27 @@
+import fieldmapRED from '../../images/fieldmapRED.png';
+import fieldmapBLUE from '../../images/fieldmapBLUE.png';
+import { ToggleButton } from '@mui/material';
+import { useState } from 'react';
+
 function MatchApp() {
+    const [currentImage, setCurrentImage] = useState(fieldmapRED);
+    const handleToggle = () => {
+        setCurrentImage(
+            currentImage === fieldmapRED ? fieldmapBLUE : fieldmapRED
+        );
+    };
+
     return (
         <>
-            <h1>Match App</h1>
+            <p>Match Scouting App</p>
+
+            <ToggleButton
+                value='check'
+                selected={currentImage === fieldmapRED}
+                onChange={handleToggle}>
+                Toggle Map Color
+            </ToggleButton>
+            <img src={currentImage} />
         </>
     );
 }
