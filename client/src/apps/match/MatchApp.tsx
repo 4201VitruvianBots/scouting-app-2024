8e92ee524afdfb2006c79f7891ee698ee3e95ee7
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-01-10 17:42:56 -0800

    app insisted I was scitzo, i proves it otherwise (read desc.)
    The app said images weren't real, I made sure they were real.

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -1,66 +1,11 @@
-import fieldmapRED from '../../images/fieldmapRED.png';
-import fieldmapBLUE from '../../images/fieldmapBLUE.png';
-import { ToggleButton } from '@mui/material';
-import { useState } from 'react';
+import Counter from "../../components/Counter";
 
 function MatchApp() {
-    const [isBlueAlliance, setIsBlueAlliance] = useState(false);
-    const currentImage = isBlueAlliance ? fieldmapBLUE : fieldmapRED; //use this boolean whenever switching from red/blue is needed
-    const [counterNear, setCounterNear] = useState(0);
-    const [counterMid, setCounterMid] = useState(0);
-    const [counterFar, setCounterFar] = useState(0);
-
-    const handleToggle = () => {
-        setIsBlueAlliance(!isBlueAlliance);
-    };
-
-    function addOneNear(){
-        setCounterNear(counterNear + 1);
-    };
-    function addOneMid(){
-        setCounterMid(counterMid + 1);
-    };
-    function addOneFar(){
-        setCounterFar(counterFar + 1);
-    };
-
-
+    
     return (
         <>
             <p>Match Scouting App</p>
-
-            <ToggleButton
-                value='check'
-                selected={currentImage === fieldmapRED}
-                onChange={handleToggle}
-                className='font-serif'>
-                Toggle Map Color
-            </ToggleButton>
-
-            {/* <div style={{ backgroundImage: `url(${currentImage})` }}> */}
-                <div
-                    style={{
-                        backgroundImage: `url(${currentImage})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        height: '40em',
-                        width: '40em',
-                        objectFit: 'contain',
-                        filter: 'brightness(80%)',
-                        alignSelf:'center'
-                        
-                    }}>
-                      
-                       <button className='bg-orange-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans'  onClick={addOneNear} id='one'>{counterNear}</button>
-                       <button className='bg-blue-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans' onClick={addOneMid} id='one'>{counterMid}</button>
-                       <button className='bg-green-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans' onClick={addOneFar} id='one'>{counterFar}</button>
-                      
-                    </div>
-                < p>hello {isBlueAlliance ? 'world' : 'natalie'}</p>
-
-            
-            {/* <img src={currentImage} className='background-image' /> */}
-            <p>Alliance: {isBlueAlliance ? 'Blue' : 'Red'}</p>
+            <Counter />
         </>
     );
 }
---
+++ client/src/components/Counter.tsx
@@ -0,0 +1,76 @@
+import fieldmapRED from '../images/fieldmapRED.png';
+import fieldmapBLUE from '../images/fieldmapBLUE.png';
+import { ToggleButton } from '@mui/material';
+import { useState } from 'react';
+
+function Counter() {
+    const [isBlueAlliance, setIsBlueAlliance] = useState(false);
+    const currentImage = isBlueAlliance ? fieldmapBLUE : fieldmapRED; //use this boolean whenever switching from red/blue is needed
+    const [counterNear, setCounterNear] = useState(0);
+    const [counterMid, setCounterMid] = useState(0);
+    const [counterFar, setCounterFar] = useState(0);
+
+    const handleToggle = () => {
+        setIsBlueAlliance(!isBlueAlliance);
+    };
+
+    function addOneNear() {
+        setCounterNear(counterNear + 1);
+    }
+    function addOneMid() {
+        setCounterMid(counterMid + 1);
+    }
+    function addOneFar() {
+        setCounterFar(counterFar + 1);
+    }
+
+    return (
+        <>
+            <ToggleButton
+                value='check'
+                selected={currentImage === fieldmapRED}
+                onChange={handleToggle}
+                className='font-serif'>
+                Toggle Map Color
+            </ToggleButton>
+
+            {/* <div style={{ backgroundImage: `url(${currentImage})` }}> */}
+            <div
+                style={{
+                    backgroundImage: `url(${currentImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '40em',
+                    width: '40em',
+                    objectFit: 'contain',
+                    filter: 'brightness(80%)',
+                    alignSelf: 'center',
+                }}>
+                <button
+                    className='h-full w-1/3 bg-orange-500 font-sans text-6xl  font-semibold text-white md:bg-opacity-50'
+                    onClick={addOneNear}
+                    id='one'>
+                    {counterNear}
+                </button>
+                <button
+                    className='h-full w-1/3 bg-blue-500 font-sans text-6xl  font-semibold text-white md:bg-opacity-50'
+                    onClick={addOneMid}
+                    id='one'>
+                    {counterMid}
+                </button>
+                <button
+                    className='h-full w-1/3 bg-green-500 font-sans text-6xl  font-semibold text-white md:bg-opacity-50'
+                    onClick={addOneFar}
+                    id='one'>
+                    {counterFar}
+                </button>
+            </div>
+            <p>hello {isBlueAlliance ? 'world' : 'natalie'}</p>
+
+            {/* <img src={currentImage} className='background-image' /> */}
+            <p>Alliance: {isBlueAlliance ? 'Blue' : 'Red'}</p>
+        </>
+    );
+}
+
+export default Counter
