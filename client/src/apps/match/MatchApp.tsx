a56ea0bc2fe59ed850c716ef9eff0ae3fd9bff22
Gabriel Chavez <specter.assassin.e2o4@gmail.com>
2024-01-10 19:02:30 -0800

    added amp button
    AMP

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -9,6 +9,7 @@ function MatchApp() {
     const [counterNear, setCounterNear] = useState(0);
     const [counterMid, setCounterMid] = useState(0);
     const [counterFar, setCounterFar] = useState(0);
+    const [AMP, setAMP] = useState(0);
 
     const handleToggle = () => {
         setIsBlueAlliance(!isBlueAlliance);
@@ -23,7 +24,9 @@ function MatchApp() {
     function addOneFar(){
         setCounterFar(counterFar + 1);
     };
-
+    function addOneAMP(){
+        setAMP(AMP + 1);
+    }
 
     return (
         <>
@@ -54,15 +57,18 @@ function MatchApp() {
                        <button className='bg-orange-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans'  onClick={addOneNear} id='one'>{counterNear}</button>
                        <button className='bg-blue-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans' onClick={addOneMid} id='one'>{counterMid}</button>
                        <button className='bg-green-500 w-1/3 h-full md:bg-opacity-50 font-semibold  text-white text-6xl font-sans' onClick={addOneFar} id='one'>{counterFar}</button>
-                      
+                       <button className='' onClick={addOneAMP} id='one'>{AMP}</button>
                     </div>
                 < p>hello {isBlueAlliance ? 'world' : 'natalie'}</p>
 
             
             {/* <img src={currentImage} className='background-image' /> */}
             <p>Alliance: {isBlueAlliance ? 'Blue' : 'Red'}</p>
+       
         </>
     );
 }
+
+
 
 export default MatchApp;
