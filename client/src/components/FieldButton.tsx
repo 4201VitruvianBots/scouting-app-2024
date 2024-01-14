831c1b6c9fbd6872f987d665a597152dc56ec09e
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-01-14 14:34:08 -0800

    prepped the map image

--- client/src/components/FieldButton.tsx
+++ client/src/components/FieldButton.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import { Button } from '@mui/material';
-// import fieldRed from '../images/fieldRed.png';
-// import fieldBlue from '../images/fieldBlue.png';
+import { Button, ToggleButton } from '@mui/material';
+import fieldRed from '../images/fieldRed.png';
+import fieldBlue from '../images/fieldBlue.png';
 
 function FieldButton() {
     const [count, setCount] = useState({ near: 0, mid: 0, far: 0, amp: 0, trap: 0, high: 0,});
     type countKeys = 'near' | 'mid' | 'far' | 'amp' | 'trap' | 'high';
-    // const [alliance, setAlliance] = useState(false); //false=blue, true=red
-    // const image = alliance ? fieldBlue : fieldRed;
+    const [alliance, setAlliance] = useState(false); //false=blue, true=red
+    const image = alliance ? fieldBlue : fieldRed;
 
     const handleCount = (key: countKeys) => {
         setCount(prevCount => ({
@@ -15,22 +15,21 @@ function FieldButton() {
             [key]: prevCount[key] + 1,
         }));
     };
-    // const handleImage = () => {
-    //     setAlliance(!alliance);
-    // };
+    const handleImage = () => {
+        setAlliance(!alliance);
+    };
 
     return (
         <div>
-            {/* <ToggleButton
+            <ToggleButton
                 value='check'
                 selected={image === fieldRed}
                 onChange={handleImage}
                 className='font-serif'>Toggle Map Color
-            </ToggleButton> */}
+            </ToggleButton>
             <Button variant='outlined' onClick={() => handleCount('near')}>{count.near}</Button>
             <Button variant='outlined' onClick={() => handleCount('mid')}>{count.mid}</Button>
             <Button variant='outlined' onClick={() => handleCount('far')}>{count.far}</Button>
-            <Button variant='outlined' onClick={() => handleCount('amp')}>{count.amp}</Button>
         </div>
     );
 }
