3e65133a09de50729730f20993357cd7538449f5
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-01-14 17:45:27 -0800

    just formatting

--- client/src/components/FieldButton.tsx
+++ client/src/components/FieldButton.tsx
@@ -4,14 +4,7 @@ import fieldRed from '../images/fieldRed.png';
 import fieldBlue from '../images/fieldBlue.png';
 
 function FieldButton() {
-    const [count, setCount] = useState({
-        near: 0,
-        mid: 0,
-        far: 0,
-        amp: 0,
-        trap: 0,
-        high: 0,
-    });
+    const [count, setCount] = useState({near: 0, mid: 0, far: 0, amp: 0, trap: 0, high: 0});
     type countKeys = 'near' | 'mid' | 'far' | 'amp' | 'trap' | 'high';
     const [alliance, setAlliance] = useState(false); //false=blue, true=red
     const image = alliance ? fieldBlue : fieldRed;
