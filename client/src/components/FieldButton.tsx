09a2d5b3c452e23c819ec54e73cfdca7000d94f2
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-01-20 14:01:14 -0800

    uncommented image

--- client/src/components/FieldButton.tsx
+++ client/src/components/FieldButton.tsx
@@ -1,15 +1,15 @@
 // Using this file to re-write other files and clean em up
 import { useState } from 'react';
 import { Button, ButtonGroup, ToggleButton } from '@mui/material';
-//import fieldRed from '../images/fieldRed.png';
-//import fieldBlue from '../images/fieldBlue.png';
+import fieldRed from '../images/fieldRed.png';
+import fieldBlue from '../images/fieldBlue.png';
 
 function FieldButton() {
     type countKeys = 'near' | 'mid' | 'far' | 'amp' | 'trap' | 'high';
     const [count, setCount] = useState({near: 0, mid: 0, far: 0, amp: 0, trap: 0, high: 0});
     const [countHistory, setCountHistory] = useState<{key: countKeys, oldValue: number, newValue: number}[]>([]);
     const [alliance, setAlliance] = useState(false); //false=blue, true=red
-    //const image = alliance ? fieldBlue : fieldRed; //sets image to Blue/Red depending on alliance
+    const image = alliance ? fieldBlue : fieldRed; //sets image to Blue/Red depending on alliance
 
     const handleCount = (key: countKeys) => {
         setCount(prevCount => {
@@ -37,7 +37,7 @@ function FieldButton() {
             <ToggleButton value='check' onClick={handleImage}>Switch Alliance</ToggleButton>
             <Button onClick={undoCount}>Undo Count</Button>
             <div>
-                {/* <img src={image} /> */}
+                <img src={image} />
                 <ButtonGroup className=''>
                     <Button onClick={() => handleCount('near')}>{count.near}</Button>
                     <Button onClick={() => handleCount('mid')}>{count.mid}</Button>
