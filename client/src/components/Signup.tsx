56e0a4c4d393fc69b51985c9d729be7f1569e8db
ashreeyaaa <148726516+mochiii-ai@users.noreply.github.com>
2024-01-23 20:09:35 -0800

    Sign In Stuffs

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -13,6 +13,7 @@ import { styled } from '@mui/material';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import MuiToggleButton from '@mui/material/ToggleButton';
 import React from 'react';
+import { Margin } from '@mui/icons-material';
 
 
 // "savedValue" is where the signin user is saved
@@ -70,38 +71,20 @@ function Signup() {
         {showPopup && (
            
             // commented code below is for preventing people from pressing buttons beyond the popup, but it also affects the opacity of he popup so its commented for now
-         //<div className='opacity-75' style={{backgroundColor: 'lightgrey', padding: '20px', position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, zIndex: 2}}>
-          <div className="rounded-xl border-green-900 border-8 text-5xl align-middle opacity-100 " style={{backgroundColor: 'lightgray', padding: '20px', position: 'fixed', top: 150, bottom: 150, left: 150, right: 150, zIndex: 1}}>
+          <div className='bg-neutral-700/75 p-5 fixed top-0 bottom-0 left-0 right-0 z-10'>
+          <div className='flex flex-col items-center rounded-xl border-green-900 border-8 text-5xl text-center align-middle bg-neutral-200 p-5 fixed top-40 bottom-40 left-40 right-40 z-10'>
             Sign in with your name please :D
             
-            <br></br>
-            <br></br>
-            <br></br>
+           
             
-            <div className="flex justify-center items-center">
-            <input className='border-1 rounded-lg border border-gray-700 text-5xl' type="text" value={inputValue} onChange={handleChangeUser}></input>
+            <div className="flex justify-center items-center mt-20">
+            <input className='border-1 rounded-lg border border-gray-700 text-4xl' type="text" value={inputValue} onChange={handleChangeUser}></input>
             </div>
-            <br></br>
 
-            <Box sx={{ maxWidth: 120 }}>
-                <FormControl fullWidth required>
-                    <InputLabel id='demo-simple-select-label'>Event</InputLabel>
-                    <Select
-                        labelId='demo-simple-select-label'
-                        id='demo-simple-select'
-                        value={event} //uhoh
-                        label='Event'
-                        onChange={handleChangeEvent}>
-                        <MenuItem value={'Port'}>Port Hueneme</MenuItem>
-                        <MenuItem value={'LAR'}>LAR</MenuItem>
-                    </Select>
-                   
-                </FormControl>
-            </Box>
-
+            <br/>
             <div className='textArea align-middle'>
                 <FormControl>
-                    <FormLabel id='demo-controlled-radio-buttons-group'>
+                    <FormLabel id='demo-controlled-radio-buttons-group' sx={{fontSize:'1em'}}>
                         Alliance
                     </FormLabel>
                 </FormControl>
@@ -112,32 +95,34 @@ function Signup() {
                     onChange={handleChangeAlliance}
                     aria-label='text alignment'>
                     <ToggleButtonRed value='Red 1' aria-label='Red 1'>
-                        <p className='font-semibold text-red-500 bg-5'>Red 1</p>
+                        <p className='font-semibold text-red-500 text-xl bg-5'>Red 1</p>
                     </ToggleButtonRed>
                     <ToggleButtonRed value='Red 2' aria-label='Red 2'>
-                        <p className='font-semibold text-red-500'> Red 2</p>
+                        <p className='font-semibold text-red-500 text-xl'> Red 2</p>
                     </ToggleButtonRed>
                     <ToggleButtonRed value='Red 3' aria-label='Red 3'>
-                        <p className='font-semibold text-red-500'>Red 3</p>
+                        <p className='font-semibold text-red-500 text-xl'>Red 3</p>
                     </ToggleButtonRed>
                     <ToggleButtonBlue value='Blue 1' aria-label='Blue 1'>
-                        <p className='font-semibold text-blue-500'>Blue 1</p>
+                        <p className='font-semibold text-blue-500 text-xl'>Blue 1</p>
                     </ToggleButtonBlue>
                     <ToggleButtonBlue value='Blue 2' aria-label='Blue 2'>
-                        <p className='font-semibold text-blue-500'>Blue 2</p>
+                        <p className='font-semibold text-blue-500 text-xl'>Blue 2</p>
                     </ToggleButtonBlue>
                     <ToggleButtonBlue value='Blue 3' aria-label='Blue 3'>
-                        <p className='font-semibold text-blue-500'>Blue 3</p>
+                        <p className='font-semibold text-blue-500 text-xl'>Blue 3</p>
                     </ToggleButtonBlue>
                 </ToggleButtonGroup>
             </div>
+            <br/>
             <div className="flex justify-center items-center">
-            <button className=' bg-green-500 font-sans text-6xl  font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-4 shadow-xl'
+            <button className=' bg-green-500 font-sans text-6xl font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-4 py-4 shadow-xl'
             onClick={handleSaveAndClose}>Sign In</button>
             </div>
+            
             <br></br>
             </div>  
-        //  </div>
+            </div>
           
         )}
         </div>
