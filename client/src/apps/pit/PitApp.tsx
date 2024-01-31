4976dfa4c263d9ec097d7773707358b72eeee609
koro <dragneelmegumi23@gmail.com>
2024-01-30 19:56:25 -0800

    small changes to pitscout
    some changes, ignore branch again :)

--- client/src/apps/pit/PitApp.tsx
+++ client/src/apps/pit/PitApp.tsx
@@ -3,25 +3,17 @@ import BackHome from "../../components/BackHome";
 import HomeIcon from '@mui/icons-material/Home';
 import MuiToggleButton from '@mui/material/ToggleButton';
 import { styled } from '@mui/material';
-import React from "react";
+import React, { useState } from "react";
+
 
 function PitApp() {
 
+  const [role, setRole] = useState<'scoring' | 'defense' | 'support' | 'all-round'>();
+  const [drivetrain, setDrivetrain] = useState<'tank' | 'swerve' | 'MECANUM' | 'other'>();
 
-    const ToggleButton1 = styled(MuiToggleButton)({
-        '&.Mui-selected, &.Mui-selected:hover': {
-            color: 'white',
-            backgroundColor: '#3268a8',
-        },
-        
-    });
 
-    const ToggleButton2 = styled(MuiToggleButton)({
-        '&.Mui-selected, &.Mui-selected:hover': {
-            color: 'white',
-            backgroundColor: '#323aa8',
-        },
-    });
+ 
+
 
     const [scoreStates, setScoreStates] = React.useState(["scoreState1", "scoreState1", "scoreState1"]);
 
@@ -61,73 +53,82 @@ function PitApp() {
        
     return (
         <>
-            <h1 className="text-center">Pit App</h1>
+            <br/>
+            <br/>
+            <h1 className="text-center text-3xl">Pit App</h1>
+            <br/>
             <BackHome
                     link='/'
+                    className="place-content-center mx-auto w-min !flex"
                     icon={
                         <HomeIcon style={{ fontSize: '30px' }} />
                     }>
                   
             </BackHome>
             <h1 className="text-center">Team Number</h1>
+            <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number"></input>
             <br></br>
 
 
 
             <h1 className="text-center">Where can they score from? Choose all that apply</h1>
-            <br></br>
+            
             <div className="place-content-center mx-auto w-min !flex pad">
-            <ToggleButton1
+            <ToggleButton
               value="scoreState1"
               aria-label="scoreState1"
               onClick={() => handleScoreChange(0)}
               sx={{ backgroundColor: scoreStates[0] === "scoreState1" ? "white" : scoreStates[0] === "scoreState2" ? "#a4ebaf" : "#FC8066" }}>
               Near
-            </ToggleButton1>
+            </ToggleButton>
 
-            <ToggleButton1
+            <ToggleButton
              value="scoreState2"
              aria-label="scoreState2"
              onClick={() => handleScoreChange(1)}
              sx={{ backgroundColor: scoreStates[1] === "scoreState1" ? "white" : scoreStates[1] === "scoreState2" ? "#a4ebaf" : "#FC8066" }}>
              Mid
-            </ToggleButton1>
+            </ToggleButton>
 
-            <ToggleButton1
+            <ToggleButton
              value="scoreState3"
              aria-label="scoreState3"
              onClick={() => handleScoreChange(2)}
              sx={{ backgroundColor: scoreStates[2] === "scoreState1" ? "white" : scoreStates[2] === "scoreState2" ? "#a4ebaf" : "#FC8066" }}>
              Far
-            </ToggleButton1>
+            </ToggleButton>
             </div>
-
+            <br></br>
 
 
 
             <h1 className="text-center">Number of batteries?</h1>
+            <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number" value='0'></input>
             <br></br>
-
             <h1 className="text-center">Auto Capability?</h1>
             <br></br>
             <h1 className="text-center">Team role?</h1>
-            <br></br>
-            <ToggleButtonGroup className='place-content-center mx-auto w-min !flex'>
-            <ToggleButton1 value='Scoring'>Scoring</ToggleButton1>
-            <ToggleButton1 value='Defense'>Defense</ToggleButton1>
-            <ToggleButton1 value='Support'>Support</ToggleButton1>
-            <ToggleButton1 value='All-Round'>All-Round</ToggleButton1>
+            
+            <ToggleButtonGroup className='place-content-center mx-auto w-min !flex' value={role} onChange={(_, value) => setRole(value)}>
+            <ToggleButton value='scoring'>Scoring</ToggleButton>
+            <ToggleButton value='defense'>Defense</ToggleButton>
+            <ToggleButton value='support'>Support</ToggleButton>
+            <ToggleButton value='all-round'>All-Round</ToggleButton>
             </ToggleButtonGroup>
+            <br></br>
             <h1 className="text-center">Drivetrain type?</h1>
-            <br></br>
-            <ToggleButtonGroup className='place-content-center mx-auto w-min !flex' exclusive>
-            <ToggleButton2 value='tank'>Tank</ToggleButton2>
-            <ToggleButton2 value='Swerve'>Swerve</ToggleButton2>
-            <ToggleButton2 value='MECANUM'>Mecanum</ToggleButton2>
-            <ToggleButton2 value='other'>Other</ToggleButton2>
+            
+            <ToggleButtonGroup className='place-content-center mx-auto w-min !flex' exclusive value={drivetrain} onChange={(_, value) => setDrivetrain(value)}>
+            <ToggleButton value='tank'>Tank</ToggleButton>
+            <ToggleButton value='swerve'>Swerve</ToggleButton>
+            <ToggleButton value='MECANUM'>Mecanum</ToggleButton>
+            <ToggleButton value='other'>Other</ToggleButton>
             </ToggleButtonGroup>
+            <br></br>
 
-            <h1 className="text-center">Anything else of note?</h1>
+          
+
+            <h1 className="text-center">Additional Notes?</h1>
             <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="text"></input>
 
 
