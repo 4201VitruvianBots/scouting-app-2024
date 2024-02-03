ae94f933cbfc97b346dc732639a7b95cf0e05ea4
natalie w <nmwpenguin@gmail.com>
2024-02-03 11:31:45 -0800

    removed high note button
    will be on superscouting instead

--- client/src/apps/match/MatchApp.tsx
+++ client/src/apps/match/MatchApp.tsx
@@ -143,12 +143,7 @@ function MatchApp() {
                 setClimbPosition={setClimbPosition}></EndgameButton>
 
             <div className='flex flex-row gap-5'>
-                <button
-                    className='border-1 my-4 h-24 w-48 rounded-lg border border-gray-700  text-xl shadow-xl'
-                    onClick={() => handleCount('high')}>
-                    {' '}
-                    High Note: {count.high}{' '}
-                </button>
+                
                 <button
                     className='border-1 my-4 h-24 w-48 rounded-lg border border-gray-700 text-xl shadow-xl'
                     onClick={() => handleCount('trap')}>
