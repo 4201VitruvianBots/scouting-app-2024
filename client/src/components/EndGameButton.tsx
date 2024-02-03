c67a8b31bc16850d7df922d96da3f1e0509bb252
natalie w <nmwpenguin@gmail.com>
2024-02-03 11:26:39 -0800

    fixed bug

--- client/src/components/EndGameButton.tsx
+++ client/src/components/EndGameButton.tsx
@@ -3,8 +3,6 @@ import { ToggleButtonGroup, styled } from '@mui/material';
 import MuiToggleButton from '@mui/material/ToggleButton';
 import { ClimbPosition } from 'requests';
 
-
-
 function EndgameButton({
     climbPosition,
     setClimbPosition,
@@ -13,7 +11,6 @@ function EndgameButton({
     setClimbPosition: Dispatch<SetStateAction<ClimbPosition>>;
 }) {
     const [allianceBlue, setAllianceBlue] = useState(false); //false=blue, true=red
-
 
     const handleImage = () => {
         setAllianceBlue(!allianceBlue);
@@ -27,20 +24,20 @@ function EndgameButton({
     });
 
     const ToggleButton2 = styled(MuiToggleButton)({
-        '&, &:hover':{
-            backgroundColor: '#a6b2bf'
+        '&, &:hover': {
+            backgroundColor: '#a6b2bf',
         },
         '&.Mui-selected, &.Mui-selected:hover': {
             backgroundColor: '#48c55c',
         },
     });
-    
+
     const handleClimbPosition = (
         _event: React.MouseEvent<HTMLElement>,
-        newClimbPosition: ClimbPosition,
+        newClimbPosition: ClimbPosition
     ) => {
         setClimbPosition(newClimbPosition);
-    }
+    };
 
     return (
         <>
@@ -56,112 +53,97 @@ function EndgameButton({
                 className={`${allianceBlue ? 'bg-field-blue-endgame' : 'bg-field-red-endgame'} h-[40em] w-[40em] bg-cover bg-center object-contain brightness-75`}>
                 {allianceBlue ? (
                     <>
-                      <ToggleButtonGroup
-                      value = {climbPosition} 
-                      exclusive
-                      onChange={handleClimbPosition}
-                      >
-                     <ToggleButton2 value="amp"
-                     sx={{
-                        position: "absolute",
-                        top: "18em",
-                        left: "22em",
-                        rotate: "60deg",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-    }} >
-                     </ToggleButton2>
-                     <ToggleButton2 value="center" 
-                     sx={{
-                        position: "absolute",
-                        top: "8em",
-                        left: "3em",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-    }}>
+                        <ToggleButtonGroup
+                            value={climbPosition}
+                            exclusive
+                            onChange={handleClimbPosition}>
+                            <ToggleButton2
+                                value='amp'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '18em',
+                                    left: '22em',
+                                    rotate: '60deg',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
+                            <ToggleButton2
+                                value='center'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '8em',
+                                    left: '3em',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
+                            <ToggleButton2
+                                value='source'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '-1em',
+                                    left: '22em',
+                                    rotate: '-60deg',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
 
-    </ToggleButton2>
-                     <ToggleButton2 value="source"
-                     sx={{
-                        position: "absolute",
-                        top: "-1em",
-                        left: "22em",
-                        rotate: "-60deg",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-    }} >
-                     </ToggleButton2>
-                     <ToggleButton2 value="parked">
-                        Parked
-                     </ToggleButton2>
-                     <ToggleButton2 value="failed">
-                        Failed</ToggleButton2>
-                      </ToggleButtonGroup>
-                      <ToggleButton2 value="none">
-                        None
-                     </ToggleButton2>
+                            <ToggleButton2 value='parked'>Parked</ToggleButton2>
+                            <ToggleButton2 value='failed'>Failed</ToggleButton2>
+                            <ToggleButton2 value='none'>None</ToggleButton2>
+                        </ToggleButtonGroup>
+                        
                     </>
                 ) : (
                     <>
-                      <ToggleButtonGroup
-                      value = {climbPosition} 
-                      exclusive
-                      onChange={handleClimbPosition}
-                      >
-                     <ToggleButton2 value="amp"  
-                     sx={{
-                        position: "absolute",
-                        top: "19em",
-                        left: "21em",
-                        rotate: "-60deg",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-    }} ></ToggleButton2>
-                     <ToggleButton2 value="center"
-                      sx={{
-                        position: "absolute",
-                        top: "8em",
-                        right: "2em",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-    }}>
+                        <ToggleButtonGroup
+                            value={climbPosition}
+                            exclusive
+                            onChange={handleClimbPosition}>
+                            <ToggleButton2
+                                value='amp'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '19em',
+                                    left: '21em',
+                                    rotate: '-60deg',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
+                            <ToggleButton2
+                                value='center'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '8em',
+                                    right: '2em',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
+                            <ToggleButton2
+                                value='source'
+                                sx={{
+                                    position: 'absolute',
+                                    top: '-4em',
+                                    left: '22em',
+                                    rotate: '60deg',
+                                    backgroundColor: 'fuchsia',
+                                    height: '29em',
+                                    width: '4em',
+                                }}></ToggleButton2>
 
-    </ToggleButton2>
-                     <ToggleButton2 value="source"
-                      sx={{
-                        position: "absolute",
-                        top: "-4em",
-                        left: "22em",
-                        rotate: "60deg",
-                    backgroundColor:'fuchsia',
-                    height: '29em',
-                    width: '4em'
-                    }}>
-
-                    </ToggleButton2>
-                     <ToggleButton2 value="parked">
-                        Parked
-                     </ToggleButton2>
-                     <ToggleButton2 value="none">
-                        None
-                     </ToggleButton2>
-                     <ToggleButton2 value="failed">
-                        Failed</ToggleButton2>
-                    
-                      </ToggleButtonGroup>
+                            <ToggleButton2 value='parked'>Parked</ToggleButton2>
+                            <ToggleButton2 value='failed'>Failed</ToggleButton2>
+                            <ToggleButton2 value='none'>None</ToggleButton2>
+                        </ToggleButtonGroup>
                     </>
                 )}
 
-               
-
                 <br />
             </div>
-           
         </>
     );
 }
