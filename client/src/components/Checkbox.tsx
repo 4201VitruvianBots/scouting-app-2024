269c11efc9b02414eaa6a5c0f559fa0e5ceff109
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-02-03 15:53:40 -0800

    Added a className prop to checkbox component

--- client/src/components/Checkbox.tsx
+++ client/src/components/Checkbox.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from "react";
 
 function Checkbox({
+    className,
     children,
     onChange,
     checked
 } : {
+    className? : string
     children? : ReactNode;
     onChange? : (value: boolean) => void;
     checked? : boolean
 }) {
     return(
-        <label><input type='checkbox' checked={checked}
+        <label className={`${className} select-none`}><input type='checkbox' checked={checked}
             onChange={event => onChange?.(event.target.checked)}/> 
             {children}
         </label>
