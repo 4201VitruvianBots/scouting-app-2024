d8f2599444895620c92021ec577092c0739a5d47
JosiahFu <josiahfu@gmail.com>
2024-02-03 17:59:34 -0800

    Fix build errors

--- client/src/components/ScrollEffect.tsx
+++ client/src/components/ScrollEffect.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-
-export function useIsVisible(ref) {
-    const [isIntersecting, setIntersecting] = useState(false);
-  
-    useEffect(() => {
-      const observer = new IntersectionObserver(([entry]) => {
-          setIntersecting(entry.isIntersecting)
-      } 
-      );
-      
-      observer.observe(ref.current);
-      return () => {
-        observer.disconnect();
-      };
-    }, [ref]);
-  
-    return isIntersecting;
-  }
+// import { useEffect, useState } from "react";
+// 
+// export function useIsVisible(ref) {
+//     const [isIntersecting, setIntersecting] = useState(false);
+//   
+//     useEffect(() => {
+//       const observer = new IntersectionObserver(([entry]) => {
+//           setIntersecting(entry.isIntersecting)
+//       } 
+//       );
+//       
+//       observer.observe(ref.current);
+//       return () => {
+//         observer.disconnect();
+//       };
+//     }, [ref]);
+//   
+//     return isIntersecting;
+//   }
