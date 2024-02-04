256f92aca93140ca7079b9ed372cc0951bd29336
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-02-03 16:17:32 -0800

    Fresh start

--- client/src/RootMenu.tsx
+++ client/src/RootMenu.tsx
@@ -2,10 +2,8 @@ import LinkButton from './components/LinkButton';
 
 function RootMenu() {
     return (
-        <main className='box-border grid h-screen auto-rows-fr grid-cols-2 grid-rows-[auto] items-stretch gap-5 p-5'>
-            <p className='col-span-2 text-center font-mono text-3xl text-green-700'>
-                Welcome to Vitruvian Scouting
-            </p>
+        <main>
+            <p>Welcome to Vitruvian Scouting</p>
             <LinkButton link='/public'>Public</LinkButton>
             <LinkButton link='/match'>Match</LinkButton>
             <LinkButton link='/super'>Super</LinkButton>
