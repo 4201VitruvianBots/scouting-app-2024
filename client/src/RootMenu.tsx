8a2850bf6a4241ec0223db03c74750b5d597dbed
Crisanto E. Carrillo <crisanto.e.carrillo@gmail.com>
2024-02-05 09:18:01 -0800

    Main page style update

--- client/src/RootMenu.tsx
+++ client/src/RootMenu.tsx
@@ -2,35 +2,35 @@ import LinkButton from './components/LinkButton';
 
 function RootMenu() {
     return (
-        <main className='grid grid-cols-1'>
-            <div className='bg-white shadow-md w-screen z-40 fixed'>
-                <h1 
-                className='text-5xl font-bold text-green-700 p-5'>
-                    Vitruvian Scouting
-                </h1>
-            </div>
-            <div className='grid grid-cols-1 gap-28 py-56 snap-y'>
+        <main className='min-h-screen bg-[#171c26] text-white text-center 
+        grid grid-cols-2 grid-rows-[auto] auto-rows-fr gap-10 pb-10 px-10'>
+            <h1 className='bg-[#2f3646] col-span-2 text-4xl text-green-700 font-bold p-5'>
+                Vitruvian Scouting
+            </h1>
                 <LinkButton link='/match'
-                className='text-8xl px-5 py-3 snap-center shadow-md text-left'>
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
                     Match
                 </LinkButton>
                 <LinkButton link='/super'
-                className='text-8xl px-5 py-3 snap-center shadow-md text-left'>
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
                     Super
                 </LinkButton>
                 <LinkButton link='/pit'
-                className='text-8xl px-5 py-3 snap-center shadow-md text-left'>
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
                     Pit
                 </LinkButton>
                 <LinkButton link='/recon'
-                className='text-8xl px-5 py-3 snap-center shadow-md text-left'>
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
                     Recon
                 </LinkButton>
                 <LinkButton link='/public'
-                className='text-8xl px-5 py-3 snap-center shadow-md text-left'>
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
                     Public
                 </LinkButton>
-            </div>
+                <LinkButton link='/'
+                className='text-5xl bg-[#2f3646] rounded-3xl'>
+                    Games
+                </LinkButton>
         </main>
     );
 }
