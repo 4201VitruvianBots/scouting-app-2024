61d26bf5298d1c1d178d89fb3e4bfad59a95c455
ashreeyaaa <148726516+mochiii-ai@users.noreply.github.com>
2024-02-06 22:16:05 -0800

    MultiButton Adjustments + Changing Toggle Buttons to Checkboxes
    need to center stuff

--- client/src/apps/pit/PitApp.tsx
+++ client/src/apps/pit/PitApp.tsx
@@ -6,6 +6,7 @@ import MultiButton from '../../components/MultiButton';
 import ToggleButton from '../../components/ToggleButton'
 import React from 'react';
 import { ClimbPosition, TeamRolePit, DrivetrainPit } from 'requests';
+import Checkbox from '../../components/Checkbox';
 
 
 function PitApp({
@@ -34,6 +35,15 @@ const handleImage = () => {
   const [role, setRole] = useState<'scoring' | 'defense' | 'support' | 'all-round'>();
   const [drivetrain, setDrivetrain] = useState<'tank' | 'swerve' | 'MECANUM' | 'other'>();
   const [scoreStates, setScoreStates] = React.useState(["scoreState1", "scoreState1", "scoreState1"]);
+
+  const inputBattery = {
+    width: '70px',
+    height: '50px',
+  };
+  const inputTeamNum = {
+    width: '150px',
+    height: '50px',
+  };
 
   const Input = () => {
     return <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-3xl text-center' type="text" placeholder="Type and #" />;
@@ -92,7 +102,7 @@ const handleImage = () => {
             
             <br/>
             <h1 className="text-center">Team Number</h1>
-            <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number"></input>
+            <input style={inputTeamNum} className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number"></input>
             <br></br>
 
 
@@ -100,6 +110,7 @@ const handleImage = () => {
             <h1 className="text-center">Where can they score from? Choose all that apply</h1>
             
             <div className="place-content-center mx-auto w-min !flex pad">
+
             {/* <ToggleButton
               value="scoreState1"
               aria-label="scoreState1"
@@ -112,7 +123,7 @@ const handleImage = () => {
              value="scoreState2"
              aria-label="scoreState2"
              onClick={() => handleScoreChange(1)}
-             sx={{ backgroundColor: scoreStates[1] === "scoreState1" ? "white" : scoreStates[1] === "scoreState2" ? "#a4ebaf" : "#FC8066" }}>
+             sx={{ backgroundColor: scoreStates[1] === "scoreState1" ? "white" : scoreStates[ 1] === "scoreState2" ? "#a4ebaf" : "#FC8066" }}>
              Mid
             </ToggleButton>
 
@@ -125,11 +136,30 @@ const handleImage = () => {
             </ToggleButton> */}
             </div>
             <br></br>
-
-
+            
+            <div className="flex items-center space-x-2">
+              <Checkbox className="form-checkbox h-5 w-5 text-blue-600" />
+              <label htmlFor="ampNotes" className="cursor-pointer select-none">Amp Notes</label>
+              </div>
+              <br/>
+              <div className="flex items-center space-x-2">
+              <Checkbox className="form-checkbox h-5 w-5 text-blue-600" />
+              <label htmlFor="ampNotes" className="cursor-pointer select-none">Speaker Notes</label>
+              </div>
+              <br/>
+              <div className="flex items-center space-x-2">
+              <Checkbox className="form-checkbox h-5 w-5 text-blue-600" />
+              <label htmlFor="ampNotes" className="cursor-pointer select-none">Trap Notes</label>
+              </div>
+              <br/>
+              <div className="flex items-center space-x-2">
+              <Checkbox className="form-checkbox h-5 w-5 text-blue-600" />
+              <label htmlFor="ampNotes" className="cursor-pointer select-none">Climbing Capability</label>
+              </div>
+              <br/>
 
             <h1 className="text-center">Number of batteries?</h1>
-            <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number" placeholder="0"></input>
+            <input style={inputBattery} className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number" placeholder="0"></input>
             <br></br>
             <h1 className="text-center">Auto Capability?</h1>
             
@@ -137,30 +167,36 @@ const handleImage = () => {
             {inputList}
             <button className="bg-lime-300 font-sans text-lg font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-2 py-2 shadow-xl place-content-center mx-auto !flex pad'" onClick={onAddBtnClick}>Add input</button>
             </div>
-            <br></br>
-            <h1 className="text-center">Team role?</h1>
-            
-             <MultiButton 
-                onChange={setRole} 
-                value={role} 
-                labels={['Scoring', 'Defense', 'Support', 'All-Round']}
-                values={['scoring', 'defense', 'support', 'all-round']}
-                className={alliance
-                ? [/*blue*/ 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex'] 
-                : [/*red*/  'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex' ]}/> 
 
+            <br></br>
+            <div className="flex flex-col items-center">
+              <h1 className="text-center">Team role?</h1>
+              <br/>
+            <div className='flex justify-center space-x-4'>
+              <MultiButton
+              onChange={setRole}
+              value={role}
+              labels={['Scoring', 'Defense', 'Support', 'All-Round']}
+              values={['scoring', 'defense', 'support', 'all-round']}
+              className={['place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex' ]}
+              />
+              </div>
+              </div>
            
             <br></br>
+            <div className='flex flex-col items-center'>
             <h1 className="text-center">Drivetrain type?</h1>
-
+            <br/>
+            <div className='flex justify-center space-x-4'>
             <MultiButton
             onChange={setDrivetrain}
             value={drivetrain}
             labels={['Tank', 'Swerve', 'Mecanum', 'Other']}
             values={['tank','swerve','MECANUM', 'other']}
-            className={alliance
-              ? [/*blue*/ ' bg-neutral-200 place-content-center mx-auto w-min !flex', ' bg-neutral-200 place-content-center mx-auto w-min !flex', ' bg-neutral-200 place-content-center mx-auto w-min !flex', ' bg-neutral-200 place-content-center mx-auto w-min !flex'] 
-              : [/*red*/  'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex' ]}/>
+            className={['place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex', 'place-content-center mx-auto w-min !flex' ]}
+            />
+            </div>
+            </div>
 
             <br></br>
 
