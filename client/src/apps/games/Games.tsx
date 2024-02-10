11e42849327e4665d65ef0868f5fe33800863180
Gavin P <gavinjpeeler@gmail.com>
2024-02-10 10:47:17 -0800

    Made requested changes

--- client/src/apps/games/Games.tsx
+++ client/src/apps/games/Games.tsx
@@ -5,7 +5,7 @@ const Games = () => {
   return <>
         <h1>Games</h1>
         <LinkButton link='/'>Home</LinkButton>
-        <iframe src="../../../ConeStacker/index.html" width={"720px"} height={"480px"}></iframe>
+        <iframe src="/ConeStacker/index.html" width={"720px"} height={"480px"}></iframe>
     </>;
 };
 
