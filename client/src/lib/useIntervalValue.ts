c2d23619d2dc4e9ae624c9e7686320e2ebdad106
JosiahFu <josiahfu@gmail.com>
2024-02-13 18:25:08 -0800

    Allow async

--- client/src/lib/useIntervalValue.ts
+++ client/src/lib/useIntervalValue.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
-function useIntervalValue<T>(callback: () => T, delay: number): T | undefined;
-function useIntervalValue<T>(callback: () => T, delay: number, initialValue: T): T;
-function useIntervalValue<T>(callback: () => T, delay: number, initialValue?: T) {
+function useIntervalValue<T>(callback: () => T | Promise<T>, delay: number): T | undefined;
+function useIntervalValue<T>(callback: () => T | Promise<T>, delay: number, initialValue: T): T;
+function useIntervalValue<T>(callback: () => T | Promise<T>, delay: number, initialValue?: T) {
     const [value, setValue] = useState(initialValue);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setValue(callback);
+        const interval = setInterval(async () => {
+            setValue(await callback());
         }, delay)
 
         return () => clearInterval(interval);
