fc49a3e8a3a67da1282801f1c888725e84e616f2
ashreeyaaa <148726516+mochiii-ai@users.noreply.github.com>
2024-02-13 20:29:21 -0800

    New buttons + margining
    New Under Chain Traversal Button + Fixed Margining + Completed Phone View?

--- client/src/apps/pit/PitApp.tsx
+++ client/src/apps/pit/PitApp.tsx
@@ -44,6 +44,7 @@ function PitApp() {
   const [trapChecked, setTrapChecked] = useState(false);
   const [speakerChecked, setSpeakerChecked] = useState(false);
   const [climbingChecked, setClimbingChecked] = useState(false);
+  const [chainTraversalChecked, setChainTraversalChecked] = useState(false);
   const [ampPrefChecked, setAmpPrefChecked] = useState(false);
   const [trapPrefChecked, setTrapPrefChecked] = useState(false);
   const [speakerPrefChecked, setSpeakerPrefChecked] = useState(false);
@@ -68,6 +69,9 @@ function PitApp() {
   };
   const handleSpeakerChange = (newSpeakerStatus) => {
     setSpeakerChecked(newSpeakerStatus);
+  };
+  const handleChainTraversalChange = (newChainTraversalStatus) => {
+    setChainTraversalChecked (newChainTraversalStatus);
   };
   const handleClimbingChange = (newClimbingStatus) => {
     setClimbingChecked(newClimbingStatus);
@@ -101,92 +105,79 @@ function PitApp() {
     return (
         <>
             <div className='bg-gray-700'>
-            <div className="border border-neutral-900 bg-gray-800">
+            <div className="border border-neutral-900 bg-gray-800 mb-7">
               <br/>
               <h1 className="text-center text-white text-3xl mb-4">Pit App</h1>
             
             </div>
 
-            <br/>
-            
-            <br/>
-            
-            <div className="flex justify-center items-center">
+
+            <div className="flex justify-center items-center mb-8">
             <div className="flex flex-col items-center bg-emerald-200 border-emerald-700 border-4 h-24 w-2/4 justify-center rounded-lg">
             <h1 className="text-center">Team Number</h1>
             <input min={0} onChange={handleTeamNumber} value={teamNumber} style={inputTeamNum} className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number" placeholder='Team#'></input>
             </div>
             </div>
+            
 
-            <br></br>
 
-
-            
-            <h1 className="text-center text-white mb-7">What can they score? Choose all that apply.</h1>
-            
-            <div className="place-content-center mx-auto w-min !flex pad">
-            
-            <br></br>
-            
-            <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={ampChecked} onChange={handleAmpChange} className="form-checkbox h-5 w-10 text-blue-600" boxClassName='h-7 w-7'/>
-              <label htmlFor="ampNotes" className="cursor-pointer text-white select-none mr-4">Amp Notes</label>
+            <h1 className="text-center text-white mb-7 ">Capabilities? Choose all that apply.</h1>
+            <div className="place-content-center mx-auto w-min !flex pad flex-wrap">
+            <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={ampChecked} onChange={handleAmpChange} className="form-checkbox h-5 w-10 text-blue-600 mr-2" boxClassName='h-7 w-7'/>
+              <label htmlFor="ampNotes" className="cursor-pointer text-white select-none mr-4 ml-5">Amp Notes</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={speakerChecked} onChange={handleSpeakerChange} className="form-checkbox h-5 w-10 text-blue-600" boxClassName='h-7 w-7'/>
-              <label htmlFor="speakerNotes" className="cursor-pointer text-white select-none mr-4">Speaker Notes</label>
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={speakerChecked} onChange={handleSpeakerChange} className="form-checkbox h-5 w-10 text-blue-600 ml-5" boxClassName='h-7 w-7'/>
+              <label htmlFor="speakerNotes" className="cursor-pointer text-white select-none mr-4 ml-5">Speaker Notes</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={trapChecked} onChange={handleTrapChange} className="form-checkbox h-5 w-10 text-blue-600" boxClassName='h-7 w-7'/>
-              <label htmlFor="trapNotes" className="cursor-pointer text-white select-none mr-4">Trap Notes</label>
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={trapChecked} onChange={handleTrapChange} className="form-checkbox h-5 w-10 text-blue-600 mr-2" boxClassName='h-7 w-7'/>
+              <label htmlFor="trapNotes" className="cursor-pointer text-white select-none mr-4 ml-5">Trap Notes</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={climbingChecked} onChange={handleClimbingChange} className="form-checkbox h-5 w-10 text-blue-600" boxClassName='h-7 w-7'/>
-              <label htmlFor="climbingCapability" className="cursor-pointer text-white select-none mr-4">Climbing Capability</label>
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={climbingChecked} onChange={handleClimbingChange} className="form-checkbox h-5 w-10 text-blue-600 ml-16" boxClassName='h-7 w-7'/>
+              <label htmlFor="climbingCapability" className="cursor-pointer text-white select-none mr-4 ml-5">Climbing Capability</label>
+              </div>
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={chainTraversalChecked} onChange={handleChainTraversalChange} className="form-checkbox h-5 w-10 text-blue-600 ml-20" boxClassName='h-7 w-7'/>
+              <label htmlFor="underChainTraversal" className="cursor-pointer text-white select-none mr-4 ml-5">Under Chain Traversal</label>
               </div>
               </div>
 
-              <h1 className="text-center mt-8 mb-3 text-white">What is their preference? Choose all that apply</h1>
-            <div className="place-content-center mx-auto w-min !flex pad">
-            <br/>
-            <div className="flex items-center whitespace-nowrap">
+
+              <h1 className="text-center mt-8 mb-3 text-white">What is their preference? Choose all that apply.</h1>
+            <div className="place-content-center mx-auto w-min !flex pad flex-wrap">
+            
+            <div className="flex items-center whitespace-nowrap mb-4">
               <Checkbox checked={ampPrefChecked} onChange={handleAmpPrefChange} className="form-checkbox h-5 w-5 text-blue-600" boxClassName='h-7 w-7' />
               <label htmlFor="ampPreferred" className="cursor-pointer text-white select-none mr-4 ml-5">Amp Preferred?</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={speakerPrefChecked} onChange={handleSpeakerPrefChange} className="form-checkbox h-5 w-5 text-blue-600" boxClassName='h-7 w-7' />
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={speakerPrefChecked} onChange={handleSpeakerPrefChange} className="form-checkbox h-5 w-5 text-blue-600 ml-7" boxClassName='h-7 w-7' />
               <label htmlFor="speakerPreferred" className="cursor-pointer text-white select-none mr-4 ml-5">Speaker Preferred?</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
+              <div className="flex items-center whitespace-nowrap mb-4">
               <Checkbox checked={trapPrefChecked} onChange={handleTrapPrefChange} className="form-checkbox h-5 w-5 text-blue-600" boxClassName='h-7 w-7' />
               <label htmlFor="trapPreferred" className="cursor-pointer text-white select-none mr-4 ml-5">Trap Preferred?</label>
               </div>
-              <br/>
-              <div className="flex items-center whitespace-nowrap">
-              <Checkbox checked={climbingPrefChecked} onChange={handleClimbingPrefChange} className="form-checkbox h-5 w-5 text-blue-600" boxClassName='h-7 w-7' />
+              <div className="flex items-center whitespace-nowrap mb-4">
+              <Checkbox checked={climbingPrefChecked} onChange={handleClimbingPrefChange} className="form-checkbox h-5 w-5 text-blue-600 ml-9" boxClassName='h-7 w-7' />
               <label htmlFor="climbPreferred" className="cursor-pointer text-white select-none mr-4 ml-5">Climbing Preferred?</label>
               </div>
+              
               </div>
 
-              
-              <br/>
-              <br/>
-
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mb-8 mt-7">
             <div className="flex flex-col items-center bg-emerald-200 border-emerald-700 border-4 h-24 w-2/4 justify-center rounded-lg ">
             <h1 className="text-center">Number of Batteries?</h1>
             <input min={0} onChange={handleBatteryNumber} value={batteryNumber} style={inputBattery} className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' type="number" placeholder="0"></input>
             </div>
             </div>
             
-            <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="ml-8 flex flex-col items-center justify-center">
               <h1 className="text-center text-white mb-4">Auto Capability?</h1>
-              <div className="flex flex-col items-center space-y-4">
+              <div className="flex flex-col items-center">
                 {autoInputValues.map((value, index) => (
                 <div key={index} className="flex items-center space-x-2">
                   <input
@@ -203,14 +194,13 @@ function PitApp() {
                   </div>
                ))}
             </div>
-            <br/>
-            <button className="bg-green-500 font-sans text-lg font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-2 py-2 shadow-xl place-content-center mx-auto !flex pad " onClick={addAnotherAuto}>Add input</button>
+            <button className="bg-green-500 font-sans text-lg font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-2 py-2 shadow-xl place-content-center mx-auto !flex pad mt-4 mb-5 " onClick={addAnotherAuto}>Add input</button>
             </div>
 
         
 
 
-            <div className="flex justify-center items-center ">
+            <div className="flex justify-center items-center mb-8">
               <div className="flex flex-col items-center bg-emerald-200 border-emerald-700 border-4 h-48 w-3/4 justify-center rounded-lg ">
                 <h1 className="text-center font-semibold mb-3">Team role?</h1>
                 <div className='grid grid-cols-2 gap-4 justify-center'>
@@ -232,8 +222,7 @@ function PitApp() {
   </div>
 </div>        
            
-            <br></br>
-            <div className="flex justify-center items-center ">
+            <div className="flex justify-center items-center mb-8">
               <div className="flex flex-col items-center bg-emerald-200 border-emerald-700 border-4 h-48 w-3/4 justify-center rounded-lg ">
                 <h1 className="text-center font-semibold mb-3">Drivetrain type?</h1>
                 <div className='grid grid-cols-2 gap-4 justify-center'>
@@ -254,12 +243,10 @@ function PitApp() {
     </div>
   </div>
 </div>
-            <br></br>
-
+            
             <h1 className="text-center text-white">Additional Notes?</h1>
-            <input className='place-content-center mx-auto w-min !flex border-1 rounded-lg border border-gray-700 text-4xl text-center' onChange={handleAdditionalNotes} value={additionalNotes} type="text"></input>
+            <input className='place-content-center mx-auto w-5/6 !flex border-1 rounded-lg border border-gray-700 text-4xl text-center mb-3' onChange={handleAdditionalNotes} value={additionalNotes} type="text"></input>
 
-            <br></br>
             <button className='bg-green-500 font-sans text-4xl font-semibold text-black md:bg-opacity-50 border-1 rounded-lg border border-gray-700 px-4 py-4 shadow-xl place-content-center mx-auto w-min !flex pad '>Submit</button>
             </div>
         </>
