b04a651d93bd38c4a3c193de7d38981d71913932
JosiahFu <josiahfu@gmail.com>
2024-02-16 20:22:19 -0800

    add :remote scripts for the remote server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,8 @@ import { app } from './server.js';
 dotenv.load({ path: '.env' });
 dotenv.load({ path: '.env.local' });
 
+const remote = process.env.LOCATION === 'remote';
+
 const container = await startDockerContainer(process.env.CONTAINER_NAME);
 
 mongoose.connect('mongodb://0.0.0.0:27107/');
@@ -15,27 +17,27 @@ const server = app.listen(8080, () => {
     console.log('Server running at http://localhost:8080');
 });
 
-try {
+if (remote) {
     const url = await ngrok.connect({ authtoken: process.env.NGROK_TOKEN, addr: 8080 });
     console.log(`Server is accessible at ${url}`);
-} catch (e) {
-    console.log(e)
 }
 
 let stopping = false;
 
-process.on('SIGINT', async () => {
+const handleExit = async () => {
     if (stopping) return;
     stopping = true;
 
     console.log('Stopping server...');
+
     server.close();
 
-    await Promise.all([
-        ngrok.disconnect(),
-        stopDockerContainer(container),
-    ]);
+    await stopDockerContainer(container);
 
     console.log('Done');
+
     process.exit();
-});
+}
+
+process.on('SIGINT', handleExit);
+process.on('SIGTERM', handleExit);
