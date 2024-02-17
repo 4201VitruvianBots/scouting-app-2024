88562519851de46ee47026b374b58c2e607a83db
JosiahFu <josiahfu@gmail.com>
2024-02-16 18:04:50 -0800

    Add ngrok proxy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
+import ngrok from 'ngrok';
+import dotenv from 'dotenv-mono';
 import { startDockerContainer, stopDockerContainer } from 'database';
 import { app } from './server.js';
+
+dotenv.load({ path: '.env' });
+dotenv.load({ path: '.env.local' });
 
 const container = await startDockerContainer(process.env.CONTAINER_NAME);
 
@@ -10,6 +15,13 @@ const server = app.listen(8080, () => {
     console.log('Server running at http://localhost:8080');
 });
 
+try {
+    const url = await ngrok.connect({ authtoken: process.env.NGROK_TOKEN, addr: 8080 });
+    console.log(`Server is accessible at ${url}`);
+} catch (e) {
+    console.log(e)
+}
+
 let stopping = false;
 
 process.on('SIGINT', async () => {
@@ -18,7 +30,11 @@ process.on('SIGINT', async () => {
 
     console.log('Stopping server...');
     server.close();
-    await stopDockerContainer(container);
+
+    await Promise.all([
+        ngrok.disconnect(),
+        stopDockerContainer(container),
+    ]);
 
     console.log('Done');
     process.exit();
