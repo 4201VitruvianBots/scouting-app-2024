2439520da95d888c4dd91fb678ca793031457e23
JosiahFu <josiahfu@gmail.com>
2024-02-19 09:30:04 -0800

    Added type satisfier for aggregation

--- server/src/aggregate.ts
+++ server/src/aggregate.ts
@@ -2,22 +2,22 @@ import { MatchDataAggregations } from "requests";
 import { matchApp } from "./Schema.js";
 
 
-async function averageAndMax():Promise<MatchDataAggregations[]>{
-    return (await matchApp.aggregate([
-    { $group:{
-        _id: {teamNumber: '$metadata.robotTeam'},
-        averageTeleSpeakerNotes: {$avg: {$add: ['$teleNotes.near', '$teleNotes.mid', '$teleNotes.far']}},
-        averageTeleAmpNotes: { $avg: '$teleNotes.amp'},
-        averageAutoSpeakerNotes: {$avg: {$add:['$autoNotes.near', '$autoNotes.mid', '$autoNotes.far']}},
-        averageAutoAmpNotes: {$avg: '$autoNotes.amp'},
-        averageTrapNotes: {$avg: '$trapNotes'},
-        maxTeleSpeakerNotes: {$max: {$add:['$teleNotes.near', '$teleNotes.mid', '$teleNotes.far']}},
-        maxTeleAmpNotes: {$max: '$teleNotes.amp'},
-        maxAutoSpeakerNotes: {$max: {$add: ['$autoNotes.near', '$autoNotes.mid', '$autoNotes.far']}},
-        maxAutoAmpNotes: {$max: '$autoNotes.amp'},
-        maxTrapNotes: {$max: '$trapNotes'}
-    }}
-]));
+async function averageAndMax(): Promise<MatchDataAggregations[]> {
+    return (await matchApp.aggregate([{
+        $group: {
+            _id: { teamNumber: '$metadata.robotTeam' },
+            averageTeleSpeakerNotes: { $avg: { $add: ['$teleNotes.near', '$teleNotes.mid', '$teleNotes.far'] } },
+            averageTeleAmpNotes: { $avg: '$teleNotes.amp' },
+            averageAutoSpeakerNotes: { $avg: { $add: ['$autoNotes.near', '$autoNotes.mid', '$autoNotes.far'] } },
+            averageAutoAmpNotes: { $avg: '$autoNotes.amp' },
+            averageTrapNotes: { $avg: '$trapNotes' },
+            maxTeleSpeakerNotes: { $max: { $add: ['$teleNotes.near', '$teleNotes.mid', '$teleNotes.far'] } },
+            maxTeleAmpNotes: { $max: '$teleNotes.amp' },
+            maxAutoSpeakerNotes: { $max: { $add: ['$autoNotes.near', '$autoNotes.mid', '$autoNotes.far'] } },
+            maxAutoAmpNotes: { $max: '$autoNotes.amp' },
+            maxTrapNotes: { $max: '$trapNotes' }
+        } satisfies { [K in keyof MatchDataAggregations]: unknown }
+    }]));
 }
 
-export { averageAndMax,} 
+export { averageAndMax, } 
