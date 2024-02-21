02a9591369712791c720af5fe37cf45b6303c644
Benjamin Chan <benjamin.v.chan@gmail.com>
2024-02-20 20:43:18 -0800

    Finalized all Data Analysis Stuff!

--- server/scripts/addFakeData.ts
+++ server/scripts/addFakeData.ts
@@ -14,36 +14,38 @@ function choose<T>(array: T[]) {
 await startDockerContainer(process.env.CONTAINER_NAME);
 await mongoose.connect('mongodb://0.0.0.0:27017/');
 
-const teams = new Array(10).fill(0).map(() => Math.floor(10000 * Math.random()));
+const teams = new Array(50).fill(0).map(() => Math.floor(10000 * Math.random()));
 
-for (let i = 0; i < 60; i++) {
-    console.log(i);
-    await new matchApp({
-        autoNotes: {
-            near: randint(5),
-            mid: randint(5),
-            far: randint(5),
-            amp: randint(5),
-            miss: randint(5)
-        },
-        climb: choose(['amp', 'center', 'failed', 'none', 'park', 'source']),
-        leftStartingZone: Math.random() > 0.5,
-        metadata: {
-            robotPosition: choose(['blue_1', 'blue_2', 'blue_3', 'red_1', 'red_2', 'red_3']),
-            robotTeam: choose(teams),
-            scouterName: 'Jim',
-            matchNumber: randint(40),
-        },
-        teleNotes: {
-            far: randint(5),
-            mid: randint(5),
-            near: randint(5),
-            amp: randint(5),
-            miss: randint(5)
-        },
-       
-        trapNotes: randint(2),
-    } satisfies MatchData).save()
+for (let matchNumber = 1; matchNumber < 400; matchNumber++) {
+    for (const robotPosition of ['red_1', 'red_2', 'red_3', 'blue_1', 'blue_2', 'blue_3'] as const) {
+        console.log(matchNumber);
+        await new matchApp({
+            autoNotes: {
+                far: randint(10),
+                mid: randint(10),
+                near: randint(10),
+                amp: randint(10),
+                miss: randint(10)
+            },
+            climb: choose(['amp', 'center', 'failed', 'none', 'park', 'source']),
+            leftStartingZone: Math.random() > 0.5,
+            metadata: {
+                robotPosition,
+                robotTeam: choose(teams),
+                scouterName: 'Jim',
+                matchNumber: matchNumber,
+            },
+            teleNotes: {
+                far: randint(10),
+                mid: randint(10),
+                near: randint(10),
+                amp: randint(10),
+                miss: randint(10)
+            },
+        
+            trapNotes: randint(2),
+        } satisfies MatchData).save()
+    }
 }
 
 await mongoose.disconnect();
