71f252b1a1adbde6fc16b66cc32f4a88d4349c38
natalie w <nmwpenguin@gmail.com>
2024-02-27 18:18:31 -0800

    match scouting logic + formatting done

--- client/src/components/EndGameButton.tsx
+++ client/src/components/EndGameButton.tsx
@@ -24,98 +24,46 @@ function EndgameButton({
             <div
                 className={`relative flex h-[40em] w-[40em] flex-row gap-2 bg-cover bg-center py-2`}>
                 <div
-                    className={`${alliance ? 'bg-field-blue-endgame' : 'bg-field-red-endgame'} ${scouterPosition === 'red_right' ? 'rotate-180' : ''} absolute top-20 flex h-[40em] w-[40em] bg-cover bg-center`}></div>
+                    className={`${alliance ? 'bg-field-blue-endgame' : 'bg-field-red-endgame'} ${scouterPosition === 'red_right' ? 'rotate-180' : ''} absolute top-20 flex h-[40em] w-[40em] bg-cover bg-center`}>
+                    <MultiButton
+                        onChange={handleClimb}
+                        value={climbPosition}
+                        labels={['', '', '']}
+                        values={['amp', 'center', 'source']}
+                        className={
+                            alliance
+                                ? [
+                                      /*blue*/ 'absolute left-[18em] top-[16em] h-[29em] w-[4em] rotate-60',
+
+                                      'absolute left-[2em] top-[6em] h-[29em] w-[4em]',
+
+                                      'absolute left-[18em] top-[-3em] h-[29em] w-[4em] -rotate-60',
+                                  ]
+                                : [
+                                      /*red*/ 'absolute left-[18em] top-[15em] h-[29em] w-[4em] -rotate-60',
+
+                                      'absolute right-[1em] top-[5.5em] h-[29em] w-[4em]',
+
+                                      'absolute bottom-4 left-[19em] top-[-4em] h-[29em] w-[4em] rotate-60',
+                                  ]
+                        }
+                    />
+                </div>
+
                 <MultiButton
                     onChange={handleClimb}
                     value={climbPosition}
-                    labels={['', '', '', 'Failed', 'None', 'Parked']}
-                    values={[
-                        'amp',
-                        'center',
-                        'source',
-                        'failed',
-                        'none',
-                        'park',
+                    labels={['Failed', 'None', 'Parked']}
+                    values={['failed', 'none', 'park']}
+                    className={[
+                        'h-[60px] w-[217px] text-4xl ',
+                        'h-[60px] w-[217px] text-4xl ',
+                        'flex-column h-[60px] w-[217px] gap-4 text-4xl  ',
                     ]}
-                    className={`
-                        ${alliance
-                            ? [
-                                  /*blue*/ 'absolute left-[18em] top-[20em] h-[29em] w-[4em] rotate-60',
-                                  'absolute left-[2em] top-[10em] h-[29em] w-[4em]',
-                                  'absolute left-[18em] top-[1em] h-[29em] w-[4em] -rotate-60',
-                                  'h-[60px] w-[217px] text-4xl ',
-                                  'h-[60px] w-[217px] text-4xl ',
-                                  'flex-column h-[60px] w-[217px] gap-4 text-4xl  ',
-                              ]
-                            : [
-                                  /*red*/ 'absolute left-[18em] top-[20em] h-[29em] w-[4em] -rotate-60',
-                                  'absolute right-[1em] top-[10em] h-[29em] w-[4em]',
-                                  'absolute bottom-4 left-[18em] top-[1em] h-[29em] w-[4em] rotate-60',
-                                  'h-[60px] w-[210px] text-4xl ',
-                                  'h-[60px] w-[210px] text-4xl ',
-                                  'h-[60px] w-[210px] text-4xl',
-                              ]}
-                              ${scouterPosition === 'red_right' ? 'rotate-180' : ''}
-                    `}
                 />
             </div>
         </>
     );
 }
-/*
-alliance
-? blue
-: red
-
-
-blue
-    "amp"
-        position: "absolute",
-        top: "18em",
-        left: "22em",
-        rotate: "60deg",
-        height: '29em',
-        width: '4em'
-
-    "center" 
-        position: "absolute",
-        top: "8em",
-        left: "3em",
-        height: '29em',
-        width: '4em'
-   
-    "source"
-        position: "absolute",
-        top: "-1em",
-        left: "22em",
-        rotate: "-60deg",
-        height: '29em',
-        width: '4em'
-red
-    "amp:"
-        position: "absolute",
-        top: "19em",
-        left: "21em",
-        rotate: "-60deg",
-        height: '29em',
-        width: '4em'
-        
-    "center"
-        position: "absolute",
-        top: "8em",
-        right: "2em",
-        height: '29em',
-        width: '4em'
-
-    "source"
-        position: "absolute",
-        top: "-4em",
-        left: "22em",
-        rotate: "60deg",
-        height: '29em',
-        width: '4em'
-
-
-*/
 
 export default EndgameButton;
