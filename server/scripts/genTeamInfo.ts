16349b0d5ea2f8e716afb1a8e18cd8b67dd8ec68
Gavin P <gavinjpeeler@gmail.com>
2024-03-04 19:15:30 -0800

    Fixing any typing

--- server/scripts/genTeamInfo.ts
+++ server/scripts/genTeamInfo.ts
@@ -2,30 +2,82 @@ import fetch from 'node-fetch';
 import { dotenvLoad } from 'dotenv-mono';
 import fs from 'fs';
 
+interface TeamData {
+    [teamNumber: string]: {
+        primaryHex: string;
+        secondaryHex: string;
+        verified: boolean;
+        avatar?: string;
+        info: TeamInfo | {
+            Error: string;
+        };
+    };
+};
+
+interface TeamInfo {
+    address: null;
+    city: string | null;
+    country: string | null;
+    gmaps_place_id: null;
+    gmaps_url: null;
+    home_championship: Record<string, string> | null;
+    key: string;
+    lat: null;
+    lng: null;
+    location_name: null;
+    motto: null;
+    name: string;
+    nickname: string;
+    postal_code: string;
+    rookie_year: number;
+    school_name: string;
+    state_prov: string;
+    team_number: number;
+    website: string | null;
+};
+
+interface AvatarData {
+    foreign_key: string;
+    preferred: boolean;
+    type: string;
+    details: {
+      base64Image: string;
+    };
+    direct_url: string;
+    view_url: string;
+  }
+
+
 dotenvLoad({ path: '.env' });
 dotenvLoad({ path: '.env.local' });
 const apiKey = process.env.API_KEY!;
 
 // Get every team number in the output_analysis.json file
-const teams = JSON.parse(fs.readFileSync('static/output_analysis.json', 'utf-8')).map((e: any) => e.teamNumber);
+const teams = JSON.parse(fs.readFileSync('static/output_analysis.json', 'utf-8')).map((e: {teamNumber: number}) => e.teamNumber);
 
 // Create a teamColors object to store the color for each team
-const teamInfo: any = {};
+const teamInfo: TeamData = {};
 
 console.log("Getting team colors...");
 
 // Get the color for each team. If it returns a 404, default to gray
 for (const team of teams) {
     const color = await fetch(`https://api.frc-colors.com/v1/team/${team}`);
-    const colorJson = await color.json();
+    const colorJson = await color.json() as TeamData;
     if (color.status === 404) {
         teamInfo[team] = {
             primaryHex: '#7f7f7f',
             secondaryHex: '#7f7f7f',
             verified: false,
+            info: {Error: "Info not loaded"},
         };
     } else {
-        teamInfo[team] = colorJson;
+        teamInfo[team] = {
+            primaryHex: colorJson[team].primaryHex,
+            secondaryHex: colorJson[team].secondaryHex,
+            verified: colorJson[team].verified,
+            info: colorJson[team].info,
+        };
     }
 }
 
@@ -38,9 +90,9 @@ for (const team of teams) {
             'X-TBA-Auth-Key': apiKey,
         },
     });
-    const avatarJson = await avatar.json();
-    if ((avatarJson as any[]).length > 0) {
-        teamInfo[team].avatar = (avatarJson as any[])[0].details.base64Image;
+    const avatarJson = await avatar.json() as AvatarData;
+    if (avatar.status !== 404) {
+        teamInfo[team].avatar = avatarJson.details.base64Image;
     }
 }
 
@@ -53,7 +105,7 @@ for (const team of teams) {
             'X-TBA-Auth-Key': apiKey,
         },
     });
-    const infoJson = await info.json();
+    const infoJson = await info.json() as TeamInfo;
     teamInfo[team].info = infoJson;
 }
 
