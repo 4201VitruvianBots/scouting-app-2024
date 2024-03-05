e2b44c9e59396eaadc869057e70e3f22a067c00c
JosiahFu <josiahfu@gmail.com>
2024-03-04 18:05:27 -0800

    Fix automatic updates, remove unnecessary filename hashes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const nonHashFiles = [
+    'woff',
+    'woff2',
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: assetInfo => {
+                    if (nonHashFiles.some(ext => assetInfo.name.endsWith(`.${ext}`)))
+                        return 'assets/[name].[ext]'
+                    return 'assets/[name]-[hash].[ext]'
+                }
+            }
+        }
+    }
 });
